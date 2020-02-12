ebcb052a5d3076471173a79df8316613a9b3f5e4
Luiz Paulo <luizpilegilppa@gmail.com>
2020-02-12 01:04:01 -0300

    create project and new examples queues

---
+++ src/app/jobs/UserReport.js
@@ -0,0 +1,12 @@
+export default {
+    key: "UserReport",
+    options: {
+        delay: 10000,
+    },
+    async handle({ data }) {
+
+        const { user } = data;
+
+        console.log(user);
+    }
+}
